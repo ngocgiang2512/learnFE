c1d9bb043763640344359b0b338590966c5d2930
ngocgiang2512 <ngocgiang21512@gmail.com>
2017-01-10 10:33:36 +0700

    RequireJS

---
+++ javascript/RequireJS/js/credits.js
@@ -0,0 +1,17 @@
+// function getCredits() {
+//   console.log("Function: getCredits");
+
+//   var credits = "100";
+//   return credits;
+// }
+
+define(function() {
+  console.log("Funtcion: getCredits");
+
+  return {
+    getCredits: function() {
+      var credits = "100";
+      return credits;
+    }
+  }
+})
