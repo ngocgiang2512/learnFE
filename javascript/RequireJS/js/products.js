33fa39643054f0e93d23776913125801a947eed5
ngocgiang2512 <ngocgiang21512@gmail.com>
2017-01-10 10:57:13 +0700

    RequireJS

---
+++ javascript/RequireJS/js/products.js
@@ -0,0 +1,13 @@
+// function reserveProduct() {
+//   console.log("Function: reserveProduct");
+//   return true;
+// }
+
+define(function(products) {
+  return {
+    reserveProduct: function() {
+      console.log("Function: reserveProduct");
+      return true;
+    }
+  }
+})
