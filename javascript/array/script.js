268d62195dd31613043db240424d0fb53f5650e3
ngocgiang2512 <ngocgiang2512@gmail.com>
2017-04-23 22:23:34 +0700

    update

--- javascript/array/script.js
+++ javascript/array/script.js
@@ -6,10 +6,13 @@ for (i = 0; i < 12; i++) {
   randomArray.push(Math.round(Math.random() * 100))
 }
 
-console.log('randomArray: ' + randomArray)
+console.log('randomArray:')
+console.log(randomArray)
 console.log('type: ' + typeof randomArray)
 console.log('length: ' + randomArray.length)
 
+
+/****************** min, max ******************/
 let max = randomArray[0]
 let min = randomArray[0]
 
@@ -27,18 +30,52 @@ randomArray.map(item => {
 console.log('max: ' + max)
 console.log('min: ' + min)
 
+/****************** filter ******************/
 lessThan50 = randomArray.filter(item => item < 50)
 moreThan50 = randomArray.filter(item => item > 50)
+
 console.log('less than 50: ' + lessThan50)
 console.log('more than 50: ' + moreThan50)
 
+/****************** sort ******************/
 let ascendingArray = randomArray.slice(0) // copy randomArray to a new object
 let descendingArray = randomArray.slice(0) // copy randomArray to a new object
 ascendingArray.sort((a,b) => (a - b))
 descendingArray.sort((a,b) => (b - a))
+
 console.log('ascendingArray :' + ascendingArray)
 console.log('descendingArray :' + descendingArray)
 
+/****************** shift ******************/
+console.log('remove the first item:')
+randomArray.shift()
+console.log(randomArray)
+
+/****************** push ******************/
+console.log('add one item to the end:')
+randomArray.push(Math.round(Math.random() * 100))
+console.log(randomArray)
+
+/****************** pop ******************/
+console.log('remove the last item:')
+randomArray.pop()
+console.log(randomArray)
+
+/****************** slice ******************/
+console.log('slice')
+let threeFirstItems = randomArray.slice(0, 3) // 0, 1, 2
+let threeElementsFromThird = randomArray.slice(2,5) // 2, 3, 4
+let fourLastItems = randomArray.slice(-4) // -3, -2, -1, last
+
+console.log('three first items: ', threeFirstItems)
+console.log('three items from the third: ', threeElementsFromThird)
+console.log('items from third to the end: ', randomArray.slice(2)) // 2, 3, 4 ... last
+console.log('four last items', fourLastItems)
+console.log('three items from last second', randomArray.slice(-4, -1)) // -3, -2, -1
+
+let randomNumber = Math.round(Math.random() * 100)
+let hasRandomNumber = randomArray.indexOf(randomNumber) > -1 ? 'yes' : 'no'
+console.log('random Array has ' + randomNumber + ' : ' + hasRandomNumber)
 
 
 
--- javascript/object/script.js
+++ javascript/object/script.js
@@ -1,14 +1,17 @@
+console.log('javascript object')
 function extend(obj, src) {
-    for (var key in src) {
-        if (src.hasOwnProperty(key)) obj[key] = src[key];
-    }
-    return obj;
+  for (var key in src) {
+    if (src.hasOwnProperty(key)) obj[key] = src[key];
+  }
+  return obj;
 }
 
 // example
-var a = { foo: true }, b = { bar: false };
+var a = { foo: true }
+var b = { foo: false, bar: false };
 var c = extend(a, b);
 
+console.log(a)
 console.log(c);
 // { foo: true, bar: false }
 
