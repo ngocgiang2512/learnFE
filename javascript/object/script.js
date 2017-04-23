ced24a894ed7410f06860ae403104ada8f1d39c3
ngocgiang2512 <ngocgiang2512@gmail.com>
2017-04-23 21:01:26 +0700

    js object

---
+++ javascript/object/script.js
@@ -0,0 +1,18 @@
+function extend(obj, src) {
+    for (var key in src) {
+        if (src.hasOwnProperty(key)) obj[key] = src[key];
+    }
+    return obj;
+}
+
+// example
+var a = { foo: true }, b = { bar: false };
+var c = extend(a, b);
+
+console.log(c);
+// { foo: true, bar: false }
+
+function extend(obj, src) {
+  Object.keys(src).forEach(function(key) { obj[key] = src[key]; });
+  return obj;
+}
