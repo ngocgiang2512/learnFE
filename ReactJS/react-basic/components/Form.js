45984fd0427f05f10f24d6112a60296b7a3bce68
ngocgiang2512 <ngocgiang21512@gmail.com>
2017-05-17 15:32:55 +0700

    react

--- ReactJS/react-basic/components/Form.js
+++ ReactJS/react-basic/components/Form.js
@@ -1,47 +1,125 @@
 import React from 'react'
 import info from '../data/personalInfo'
 
+let years = []
+let months = []
+let now = new Date()
+let year = now.getFullYear()
+
+for (let i = year; i > year - 100; i--){
+  years.push(i)
+}
+
+for (let i = 1; i < 13; i++) {
+  months.push(i)
+}
+
 class ReactForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       data: info
     }
-    this.changeName = this.changeName.bind(this)
-    this.printName = this.printName.bind(this)
+    this.changeTextInput = this.changeTextInput.bind(this)
+    this.changeRadioInput = this.changeRadioInput.bind(this)
+    this.changeSelectInput = this.changeSelectInput.bind(this)
+    this.printInfo = this.printInfo.bind(this)
   }
 
-  changeName(e) {
+  changeTextInput(e) {
     let info = this.state.data
     info[e.target.name] = e.target.value
     this.setState({data: info})
   }
 
-  printName() {
+  changeRadioInput(e) {
+    let info = this.state.data
+    if(e.target.checked){
+      info[e.target.name] = e.target.value
+      this.setState({data: info})
+    }
+  }
+
+  changeSelectInput(e) {
+    let info = this.state.data
+    info[e.target.name] = e.target.value
+    this.setState({data: info})
+  }
+
+  getDaysInMonth() {
     let data = this.state.data
-    console.log(data.lastName + ' ' + data.middleName + ' ' + data.firstName)
+    let year = data.year
+    let month = data.month
+    let maxDayInMonth = null
+    let days = []
+
+    let thirtyDaysMonths = [4, 6, 9, 11]
+
+    if (year === '' || month === '') {
+      maxDayInMonth = 31
+    } else if (month === '2' && year % 4 !== 0) {
+      maxDayInMonth = 28
+    } else if (month === '2' && year % 4 == 0) {
+      maxDayInMonth = 29
+    } else if (thirtyDaysMonths.indexOf(parseInt(month)) > -1) {
+      maxDayInMonth = 30
+    } else {
+      maxDayInMonth = 31
+    }
+
+    for (let i = 1; i <= maxDayInMonth; i++) {
+      days.push(i)
+    }
+
+    return days
+  }
+
+  printInfo() {
+    let data = this.state.data
+    console.log(data)
   }
 
   render() {
     let data = this.state.data
+    let days = this.getDaysInMonth()
+
     return (
       <div>
         <p>React Form</p>
         <div className="form-wrapper">
           <div className="row clearfix">
-            <p className="label">First Name</p>
-            <input type="text" name="firstName" className="value" value={data.firstName} onChange={this.changeName}/>
+            <p className="label">Name</p>
+            <input type="text" name="lastName" placeholder="Last Name" value={data.lastName} onChange={this.changeTextInput}/>
+            <input type="text" name="middleName" placeholder="Middle Name" value={data.middleName} onChange={this.changeTextInput}/>
+            <input type="text" name="firstName" placeholder="First Name" value={data.firstName} onChange={this.changeTextInput}/>
           </div>
           <div className="row clearfix">
-            <p className="label">Middle Name</p>
-            <input type="text" name="middleName" className="value" value={data.middleName} onChange={this.changeName}/>
+            <p className="label">Gender</p>
+            <input type="radio" name="sex" value="male" checked={data.sex ==="male"} onChange={this.changeRadioInput}/>
+            Male
+            <input type="radio" name="sex" value="female" checked={data.sex ==="female"} onChange={this.changeRadioInput}/>
+            Female
           </div>
           <div className="row clearfix">
-            <p className="label">Last Name</p>
-            <input type="text" name="lastName" className="value" value={data.lastName} onChange={this.changeName}/>
+            <p className="label">Birthday</p>
+            Day
+            <select type="select" name="day" value={data.day} onChange={this.changeSelectInput}>
+              <option value="">--</option>
+              {days.map((day, index) => <option key={index} value={day}>{day}</option>)}
+            </select>
+            Month
+            <select type="select" name="month" value={data.month} onChange={this.changeSelectInput}>
+              <option value="">--</option>
+              {months.map((month, index) => <option key={index} value={month}>{month}</option>)}
+            </select>
+            Year
+            <select type="select" name="year" value={data.year} onChange={this.changeSelectInput}>
+              <option value="">--</option>
+              {years.map((year, index) => <option key={index} value={year}>{year}</option>)}
+            </select>
           </div>
         </div>
-        <button type="button" onClick={this.printName}>Print Name</button>
+        <button type="button" onClick={this.printInfo}>Print Information</button>
       </div>
     )
   }
--- ReactJS/react-basic/data/personalInfo.js
+++ ReactJS/react-basic/data/personalInfo.js
@@ -2,7 +2,8 @@ export default {
   firstName: 'Nam',
   middleName: 'Van',
   lastName: 'Nguyen',
-  oldID: '012546137',
-  currentID: '001087009472',
-  gender: 'male'
+  sex: 'male',
+  year: '',
+  month: '',
+  day: ''
 }
