a26ec975a726cf4d84c0b2fa8d6170e914b15c74
ngocgiang2512 <ngocgiang2512@gmail.com>
2017-06-05 21:45:08 +0700

    dependency

---
+++ Documents/dependency.js
@@ -0,0 +1,14 @@
+// react: a javascript library for building user interfaces
+// https://github.com/facebook/react
+
+/* ------------------------------------------ */
+
+// redux: predictable state container fo Javascript apps
+// https://github.com/reactjs/redux
+
+/* ------------------------------------------ */
+
+// react-redux: official react bindings for redux
+// https://github.com/reactjs/react-redux
+
+/* ------------------------------------------ */
