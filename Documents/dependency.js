7b7d9dd18f5028e0136b7ab845584c3f4ea88b09
ngocgiang2512 <ngocgiang21512@gmail.com>
2017-06-08 14:11:23 +0700

    update

--- Documents/dependency.js
+++ Documents/dependency.js
@@ -1,3 +1,16 @@
+react
+redux
+react-redux
+
+css-loader
+style-loader
+
+extract-text-webpack-plugin
+html-webpack-plugin
+
+webpack-dev-middleware
+webpack-hot-middleware
+
 // react: a javascript library for building user interfaces
 // https://github.com/facebook/react
 
@@ -15,4 +28,24 @@
 
 // css-loader: outputs the CSS as JS modules
 // style-loader: adds CSS to the DOM by injecting a <style> tag
+
+/* ------------------------------------------ */
+
+// extract text webpack plugin: Extract text from bundle into a file
+// https://github.com/webpack-contrib/extract-text-webpack-plugin
 // ExtractTextWebpackPlugin: extracts the bundled CSS and outputs CSS files
+
+/* ------------------------------------------ */
+
+// html webpack plugin: Simplifies creation of HTML files to serve your webpack bundles
+// https://github.com/jantimon/html-webpack-plugin
+
+/* ------------------------------------------ */
+
+// webpack-dev-middleware: Offers a dev middleware for webpack, which arguments a live bundle to a directory
+//https://github.com/webpack/webpack-dev-middleware
+
+/* ------------------------------------------ */
+
+// webpack-hot-middleware: Webpack hot reloading you can attach to your own server
+// https://github.com/glenjamin/webpack-hot-middleware
--- Documents/node.js
+++ Documents/node.js
@@ -18,3 +18,25 @@ bar();
 // The path module provides utilities for working with file and directory paths. It can be accessed using:
 cons path = require('path');
 
+// path.resolve([...paths])
+path.resolve('/foo/bar', './baz');
+// Returns: '/foo/bar/baz'
+
+path.resolve('/foo/bar', '/tmp/file/');
+// Returns: '/tmp/file'
+
+path.resolve('wwwroot', 'static_files/png/', '../gif/image.gif');
+// if the current working directory is /home/myself/node,
+// this returns '/home/myself/node/wwwroot/static_files/gif/image.gif'
+
+/* ------------------------------------------ */
+
+// __dirname
+// https://nodejs.org/api/globals.html
+// The directory name of the current module. This the same as the path.dirname() of the __filename.
+// Example: running node example.js from /Users/mjr
+console.log(__dirname);
+// Prints: /Users/mjr
+console.log(path.dirname(__filename));
+// Prints: /Users/mjr
+
